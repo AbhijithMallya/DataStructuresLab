361d2616ee8423dfc1de5e3a95e044690de50d67
= <=>
2022-03-02 11:38:56 +0530

    Modified 11 and 12

--- Lab12.c
+++ Lab12.c
@@ -15,49 +15,50 @@ linear probing.
 int key,n,m,*ht,hi,elec,flag;
 void createht()
 {
-int i;
-ht = (int*)malloc(m*sizeof(int));
-if(m==0)
-{
-printf("Unable to create the hash table\n");
-exit(0);
-}
-else
-for(i=0; i<m; i++)
-ht[i] = -1;
+    int i;
+    ht = (int*)malloc(m*sizeof(int));
+    if(m==0)
+    {
+    printf("Unable to create the hash table\n");
+    exit(0);
+    }
+    else
+    for(i=0; i<m; i++)
+    ht[i] = -1;
 }
 void insertht(int key)
 {
-hi = key % m;
-while(ht[hi] != -1)
-{
-hi = (hi+1)%m;
-flag = 1;
-}
-if(flag)
-{
-printf("Collision Detected and avoided by Linear Probing!\n");
-flag = 0;
-}
-ht[hi] = key;
-elec++;
+    hi = key % m;
+    while(ht[hi] != -1)
+    {
+    hi = (hi+1)%m;
+    flag = 1;
+    }
+    if(flag)
+    {
+    printf("Collision Detected and avoided by Linear Probing!\n");
+    flag = 0;
+    }
+    ht[hi] = key;
+    elec++;
 }
 void displayht()
 {
-int i;
-if(elec == 0)
-{
-printf("Hash Table is empty\n");
-return;
-}
-printf("Hash Table contents are:\n");
-for(i=0; i<m; i++)
-printf("[%d] --> %d\n", i, ht[i]);
+    int i;
+    if(elec == 0)
+    {
+    printf("Hash Table is empty\n");
+    return;
+    }
+    printf("Hash Table contents are:\n");
+    for(i=0; i<m; i++)
+    printf("[%d] --> %d\n", i, ht[i]);
 
 }
 void main()
 {
 int i;
+printf("\n%d\n",elec);
 printf("Enter the number of employee records: ");
 scanf("%d", &n);
 printf("Enter the two digit memory locations: ");
@@ -66,13 +67,13 @@ createht();
 printf("Enter four digit key values of Employee records\n");
 for(i=0; i<n; i++)
 {
-scanf("%d", &key);
-if(elec == m)
-{
-printf("Hash table is full.\n");
-break;
-}
-insertht(key);
+    scanf("%d", &key);
+    if(elec == m)
+    {
+    printf("Hash table is full.\n");
+    break;
+    }
+    insertht(key);
 }
 displayht();
 }
